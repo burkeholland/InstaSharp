0427cae67305170f607f54bae03f7b20750750ce
Josh Santangelo <josh@stimulant.io>
2013-05-11 18:37:46 -0700

    handle likes in pagination

--- Model/Pagination.cs
+++ Model/Pagination.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace InstaSharp.Model {
-    public class Pagination {
+namespace InstaSharp.Model
+{
+    public class Pagination
+    {
 
         [JsonMapping("next_url", JsonMapping.MappingType.Primitive)]
         public string NextUrl { get; set; }
-        
+
         [JsonMapping("next_max_id", JsonMapping.MappingType.Primitive)]
         public string NextMaxId { get; set; }
+
+        [JsonMapping("next_max_like_id", JsonMapping.MappingType.Primitive)]
+        public string MaxLikeId { get; set; }
     }
 }
