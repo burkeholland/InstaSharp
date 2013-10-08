f3cad289c4120831554014eba241b75d4e8563b6
Enno <mail@enricomeinel.de>
2013-10-08 10:53:36 +0200

    added gzip handling for faster response

--- HttpClient.cs
+++ HttpClient.cs
@@ -1,82 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Net;
 using System.IO;
 using System.Collections.Specialized;
 
-namespace InstaSharp {
-    public static class HttpClient {
-        public static string GET(string uri) {
-            try {
+namespace InstaSharp
+{
+    public static class HttpClient
+    {
+        public static string GET(string uri)
+        {
+            try
+            {
                 var request = HttpWebRequest.Create(uri);
                 request.Method = "GET";
-
-                return ReadResponse(request.GetResponse().GetResponseStream());
+                request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
+                var response = request.GetResponse();
+                var responseStream = UnzipResponse(response);
+                return ReadResponse(responseStream);
             }
-            catch (WebException ex) {
-                return ReadResponse(ex.Response.GetResponseStream());
+            catch (WebException ex)
+            {
+                //exceptions can also be zipped
+                var response = ex.Response;
+                var responseStream = UnzipResponse(response);
+                return ReadResponse(responseStream);
             }
         }
 
-        public static string POST(string url) {
+        public static string POST(string url)
+        {
             return POST(url, new Dictionary<string, string>());
         }
 
-        public static string POST(string url, IDictionary<string, string> args) {
-            try {
-                NameValueCollection parameters = new NameValueCollection();
-                foreach (var arg in args) {
-                    parameters.Add(arg.Key, arg.Value);
-                }
-
-                WebClient client = new WebClient();
-
-                var result = client.UploadValues(url, "POST", parameters);
-
-                return Encoding.Default.GetString(result);
+        public static string POST(string url, IDictionary<string, string> args)
+        {
+            try
+            {
+                return UploadValues(url, "POST", args);
             }
-            catch (WebException ex) {
-                return ReadResponse(ex.Response.GetResponseStream());
+            catch (WebException ex)
+            {
+                //exceptions can also be zipped
+                var response = ex.Response;
+                var responseStream = UnzipResponse(response);
+                return ReadResponse(responseStream);
             }
         }
 
-        public static string DELETE(string uri) {
-            var request = HttpWebRequest.Create(uri);
-            request.Method = "DELETE";
-
+        public static string DELETE(string uri)
+        {
             return DELETE(uri, new Dictionary<string, string>());
-
-            // return ReadResponse(request.GetResponse().GetResponseStream());
         }
 
-        public static string DELETE(string uri, IDictionary<string, string> args) {
-            try {
-                NameValueCollection parameters = new NameValueCollection();
-                foreach (var arg in args) {
-                    parameters.Add(arg.Key, arg.Value);
+        public static string DELETE(string uri, IDictionary<string, string> args)
+        {
+            try
+            {
+                return UploadValues(uri, "DELETE", args);
+            }
+            catch (WebException ex)
+            {
+                //exceptions can also be zipped
+                var response = ex.Response;
+                var responseStream = UnzipResponse(response);
+                return ReadResponse(responseStream);
+            }
+        }
+
+        private static string UploadValues(string uri, string method, IDictionary<string, string> args)
+        {
+            NameValueCollection parameters = new NameValueCollection();
+            foreach (var arg in args)
+            {
+                parameters.Add(arg.Key, arg.Value);
+            }
+            WebClient client = new WebClient();
+            client.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
+            //fetch
+            var resultBytes = client.UploadValues(uri, method, parameters);
+            //passing response
+            Stream responseStream = new MemoryStream(resultBytes);
+            try
+            {
+                if (client.ResponseHeaders[HttpResponseHeader.ContentEncoding].Equals("gzip") ||
+                    client.ResponseHeaders[HttpResponseHeader.ContentEncoding].Equals("deflate"))
+                {
+                    responseStream = new GZipStream(responseStream, CompressionMode.Decompress);
                 }
-
-                WebClient client = new WebClient();
-
-                var result = client.UploadValues(uri, "DELETE", parameters);
-
-                return Encoding.Default.GetString(result);
             }
-            catch (WebException ex) {
-                return ReadResponse(ex.Response.GetResponseStream());
+            catch (Exception) {/*no zipped response*/ }
+            StringBuilder result = new StringBuilder();
+            using (var sr = new StreamReader(responseStream))
+            {
+                while (!sr.EndOfStream)
+                {
+                    result.AppendLine(sr.ReadLine()); //better way to hold up line breaks from response
+                }
             }
+            return result.ToString();
         }
 
-        private static string ReadResponse(Stream response) {
+        private static string ReadResponse(Stream response)
+        {
             StreamReader reader = new StreamReader(response);
             string line;
             StringBuilder result = new StringBuilder();
-            while ((line = reader.ReadLine()) != null) {
-                result.Append(line);
+            while ((line = reader.ReadLine()) != null)
+            {
+                result.AppendLine(line); //better way to hold up line breaks from response
             }
+            response.Close();
             return result.ToString();
+        }
+
+        private static Stream UnzipResponse(WebResponse response)
+        {
+            Stream responseStream = response.GetResponseStream();
+            try
+            {
+                if (response.Headers[HttpRequestHeader.AcceptEncoding].Equals("gzip") ||
+                    response.Headers[HttpRequestHeader.AcceptEncoding].Equals("deflate"))
+                {
+                    responseStream = new GZipStream(responseStream, CompressionMode.Decompress);
+                }
+            }
+            catch (Exception) { /*no zipped response*/ }
+            return responseStream;
         }
     }
 }
